1e17f3c776f9b4956b3156217991c136372afa94
Regyl1 <roma.orsha@mail.ru>
2021-11-07 01:02:52 +0300

    Fix some classes, Add 4 Lab

---
+++ JS/script1.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("")
+  for (let i = 0; i < document.getElementsByClassName('header-nav__elements').length; i++){
+    if (document.getElementsByClassName('header-nav__elements')[i]["href"] === document.URL){
+      activeElement = document.getElementsByClassName('header-nav__elements')[i];
+      activeElement.className = 'active block';
+      activeElement.removeAttribute("href");
+      break;
+    }
+  }
+  });
